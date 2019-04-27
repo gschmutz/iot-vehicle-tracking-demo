bbbe72560771ff73462d3d4b05ce1657a96dff36
Guido Schmutz <guido.schmutz@trivadis.com>
2019-04-27 21:03:38 +0200

    Update start-connect-mqtt-confluent.sh

--- scripts/start-connect-mqtt-confluent.sh
+++ scripts/start-connect-mqtt-confluent.sh
@@ -18,10 +18,10 @@ curl -X "POST" "$DOCKER_HOST_IP:8083/connectors" \
     "mqtt.clean.session.enabled":"true",
     "mqtt.connect.timeout.seconds":"30",
     "mqtt.keepalive.interval.seconds":"60",
-    "mqtt.qos":"0"
+    "mqtt.qos":"0",
     "kafka.topic":"truck_position",
-    "confluent.topic.bootstrap.servers": "broker-1:9092",
-    "confluent.topic.replication.factor": "3",
+    "confluent.topic.bootstrap.servers": "broker-1:9092,broker-2:9093",
+    "confluent.topic.replication.factor": "3"
     }
   }'
 
